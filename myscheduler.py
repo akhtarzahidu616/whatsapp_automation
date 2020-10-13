60c7dd1a091ee860d8974fad8f8cb9c8d578b036
akhtarzahidu616 <72804188+akhtarzahidu616@users.noreply.github.com>
2020-10-13 19:02:01 +0530

    Add files via upload

---
+++ myscheduler.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from whatsapp_bot import automation_function
+#def job_function():
+    #print("Hello World")
+
+sched = BlockingScheduler()
+
+# Schedule job_function to be called every two hours
+sched.add_job(automation_function, 'interval', seconds=1 )
+
+sched.start()
---
+++ whatsapp_bot.py
@@ -0,0 +1,14 @@
+from twilio.rest import Client
+
+account_sid = 'AC1c9eeaf88c45b4517989fe6c922bf99b'
+auth_token = '9522958e5a84eb100c6bcbe07ab2622d'
+client = Client(account_sid, auth_token)
+
+def automation_function():
+    message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        #body='This is my Whatsapp BOT for sending automated messages',
+        body='hey u ... whats up',
+        to='whatsapp:+917008453007'
+    )
+    print(message.sid)
