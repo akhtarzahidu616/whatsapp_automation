38ba05954b453706023882eedba1c80407587620
akhtarzahidu616 <72804188+akhtarzahidu616@users.noreply.github.com>
2020-12-08 21:52:35 +0530

    Update whatsapp_bot.py

--- whatsapp_bot.py
+++ whatsapp_bot.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
 account_sid = 'AC1c9eeaf88c45b4517989fe6c922bf99b'
-auth_token = '9522958e5a84eb100c6bcbe07ab2622d'
+auth_token = 'a3db08ee21186e2b17cf2f8e71e60d9b'
 client = Client(account_sid, auth_token)
 
 def automation_function():
@@ -9,6 +9,6 @@ def automation_function():
         from_='whatsapp:+14155238886',
         body='This is my Whatsapp BOT for sending automated messages',
         #body='hey u ... whats up',
-        to='whatsapp:+917008453007'
+        to='whatsapp:+917381788563'
     )
     print(message.sid)
